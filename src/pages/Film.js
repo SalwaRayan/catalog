b90f5800f07e6ff4cefead911c1febe540ad3cd7
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-11-15 16:54:33 +0100

    done

--- src/pages/Film.js
+++ src/pages/Film.js
@@ -20,7 +20,7 @@ class Film extends Component {
 
         {filteredFilms.map((film) => (
           <>
-            <h2 className="card-title text-center mb-5">{film.title}</h2>
+            <h2 className="card-title text-center mb-3">{film.title}</h2>
             <img src={film.image} className="card-img img-fluid img-thumbnail my-3"/>
             <h4>Director: <strong>{film.director}</strong></h4>
             <h5>Cast: </h5>
@@ -29,10 +29,8 @@ class Film extends Component {
               <li>{film.stars[1]}</li>
               <li>{film.stars[2]}</li>
             </ul>
-            <div className="card-body">
-              <h5 className="card-title">Synapsis:</h5>
-              <p className="card-text">>{film.description}</p>
-            </div>
+            <h5 className="card-title">Synapsis:</h5>
+            <p className="card-text">{film.description}</p>
           </>
         ))}
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,11 @@ import films from '../film.json'
 class Home extends Component {
   render() {
     return (
-      <section>
-        <h1>List of Film</h1>
+      <section className="text-center">
+        <h1 className="my-5">List of Film</h1>
         {films.map(film => (
           <Link to={`/Film/${film.id}`}>
-            <h3>{film.title}</h3>
+            <h3 className="my-2">{film.title}</h3>
           </Link>
         ))}
       </section>
