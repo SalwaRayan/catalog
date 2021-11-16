04b8f9ce953d4b634cd594f09abbcfa8a7aa9e4e
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-11-16 09:39:51 +0100

    bit of css page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import ReturnHomePage from '../components/ReturnHomePage';
 
 class PageNotFound extends Component {
   render() {
     return (
-      <div>
+      <div className="text-center">
         <h1>404 - Error</h1>
         <p>Page not found</p>
+        <ReturnHomePage />
       </div>
     );
   }
